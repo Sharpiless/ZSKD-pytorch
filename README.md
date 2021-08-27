3ee883e1df2618d6fab6c5a8424e56c76b52efd1
Sharpiless <1691608003@qq.com>
2021-08-27 11:17:37 +0800

    更新蒸馏损失

---
+++ README.md
@@ -0,0 +1,61 @@
+# Zero-Shot Knowledge Distillation in Deep Networks Pytorch
+
+![Python version support](https://img.shields.io/badge/python-3.6-blue.svg)
+![PyTorch version support](https://img.shields.io/badge/pytorch-1.7.0-red.svg)
+
+:star: Star us on GitHub — it helps!!
+
+PyTorch implementation for *[Zero-Shot Knowledge Distillation in Deep Networks](https://arxiv.org/abs/1905.08114)*
+
+
+## Install
+
+You will need a machine with a GPU and CUDA installed.  
+Then, you prepare runtime environment:
+
+   ```shell
+   pip install -r requirements.txt
+   ```
+
+
+## Use
+
+For mnist dataset,
+
+```shell
+python main.py --dataset=mnist --t_train=False --num_sample=12000 --batch_size=200 
+```
+
+For cifar10 dataset,
+
+```shell
+python main.py --dataset=cifar10 --t_train=True --num_sample=24000 --batch_size=100
+```
+
+
+Arguments:
+
+- `dataset` - available dataset: ['mnist', 'cifar10', 'cifar100']
+- `t_train` - Train teacher network?? 
+	- if True, train teacher network
+	- elif False, load trained teacher network
+- `num_sample` - Number of DIs crafted per category
+- `beta` - Beta scaling vectors
+- `batch_size` - batch size
+- `lr` - learning rate
+- `iters` - iteration number
+- `s_save_path` - save path for student network
+- `do_genimgs` - generate synthesized images from ZSKD??
+	- if True, generate images
+	- elif False, you must have the synthesized images that are generated from ZSKD
+
+
+## Result examples for MNIST dataset
+
+![2](./assets/fig1.png)
+
+
+## Understanding this method(algorithm)
+
+:white_check_mark: Check my blog!!
+[Here](https://da2so.github.io/2020-08-12-Zero_Shot_Knowledge_Distillation_in_Deep_Networks/)
---
+++ trainer/lenet.py
@@ -0,0 +1,93 @@
+import torch.nn as nn
+import torch
+
+
+def truncated_normal_(tensor, mean=0, std=0.09):
+    with torch.no_grad():
+        size = tensor.shape
+        tmp = tensor.new_empty(size+(4,)).normal_()
+        valid = (tmp < 2) & (tmp > -2)
+        ind = valid.max(-1, keepdim=True)[1]
+        tensor.data.copy_(tmp.gather(-1, ind).squeeze(-1))
+        tensor.data.mul_(std).add_(mean)
+        return tensor
+
+
+class LeNet5(nn.Module):
+
+    def __init__(self):
+        super(LeNet5, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=5)
+        self.relu3 = nn.ReLU()
+        self.fc1 = nn.Linear(120, 84)
+        self.relu4 = nn.ReLU()
+        self.fc2 = nn.Linear(84, 10)
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
+                truncated_normal_(m.weight, std=0.1)
+
+    def forward(self, img, out_feature=False):
+        output = self.conv1(img)
+        output = self.relu1(output)
+        output = self.maxpool1(output)
+        output = self.conv2(output)
+        output = self.relu2(output)
+        output = self.maxpool2(output)
+        output = self.conv3(output)
+        output = self.relu3(output)
+        feature = output.view(-1, 120)
+        output = self.fc1(feature)
+        output = self.relu4(output)
+        output = self.fc2(output)
+        if out_feature == False:
+            return output
+        else:
+            return output, feature
+
+
+class LeNet5Half(nn.Module):
+
+    def __init__(self):
+        super(LeNet5Half, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 3, kernel_size=(5, 5))
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv2 = nn.Conv2d(3, 8, kernel_size=(5, 5))
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.conv3 = nn.Conv2d(8, 60, kernel_size=(5, 5))
+        self.relu3 = nn.ReLU()
+        self.fc1 = nn.Linear(60, 42)
+        self.relu4 = nn.ReLU()
+        self.fc2 = nn.Linear(42, 10)
+
+        # 默认方法
+        for m in self.modules():
+            if isinstance(m, (nn.Conv2d, nn.Linear)):
+                truncated_normal_(m.weight, std=0.1)
+
+    def forward(self, img, out_feature=False):
+        output = self.conv1(img)
+        output = self.relu1(output)
+        output = self.maxpool1(output)
+        output = self.conv2(output)
+        output = self.relu2(output)
+        output = self.maxpool2(output)
+        output = self.conv3(output)
+        output = self.relu3(output)
+        feature = output.view(-1, 60)
+        output = self.fc1(feature)
+        output = self.relu4(output)
+        output = self.fc2(output)
+        if out_feature == False:
+            return output
+        else:
+            return output, feature
